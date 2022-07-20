551f6e73f012fe56041767c30b5170d09d48e799
Jabes <jabesr@gmail.com>
2022-07-20 14:25:01 -0300

    Updates

---
+++ precache-manifest.dd772ba440396498cb529af5b7bb9400.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5c922d93c7ac7a755d28",
+    "url": "./static/css/main.b9a4763a.chunk.css"
+  },
+  {
+    "revision": "5c922d93c7ac7a755d28",
+    "url": "./static/js/main.5f1b9afb.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "863ed4088d2dc3d4e193",
+    "url": "./static/js/2.c5da6ee8.chunk.js"
+  },
+  {
+    "revision": "4e444e05165e0715f3b7cf4a6ffe5360",
+    "url": "./index.html"
+  }
+];
